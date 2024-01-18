a8d5bafd006451dc95ac9d35711ff4fcae0a1e9c
IsaDonutCat <114428942+IsaDonutCat@users.noreply.github.com>
2024-01-18 18:16:30 +0000

    began working on arraylist of students

--- Tester.java
+++ Tester.java
@@ -3,25 +3,41 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Tester
 {
+    public static ArrayList<Student> seniors = new ArrayList<Student>();
+    public static ArrayList<Seminar> semis = new ArrayList<Seminar>();
+
     public static void main (String[] args)
     {
         try
         {
-            if (args.length != 1)
+            if (args.length != 2)
             {
-                System.out.println("java Tester data.csv");
+                System.out.println("java Tester students.csv seminars.csv");
                 return;
             }
 
-            File rawData = new File(args[0]);
-            Scanner inptr = new Scanner(rawData);
+            File rawStudents = new File(args[0]);
+            Scanner inptr = new Scanner(rawStudents);
+            String[] row;
 
             while (inptr.hasNextLine())
             {
-                System.out.println(inptr.nextLine());
+                row = inptr.nextLine().split(",");
+                if (row.length == 7)
+                    seniors.add(new Student(row[0], row[1], row[2], row[3], row[4], row[5], row[6]));
+                else
+                    seniors.add(new Student(row[0], row[1]));
             }
 
             inptr.close();
+
+            File rawSeminars = new File(args[1]);
+            inptr = new Scanner(rawSeminars);
+
+            while (inptr.hasNextLine())
+            {
+                
+            }
         }
         catch(FileNotFoundException e)
         {
