736b3851459ca4bfde3c12d5e61c71ee3e9cce4c
IsaDonutCat <114428942+IsaDonutCat@users.noreply.github.com>
2024-03-08 11:13:27 -0500

    electric boogaloo squared

--- Tester.java
+++ Tester.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class Tester
 {
+
+    /**
+     * Main method
+     * Should call and initialize everything
+     * @param args
+     */
     public static ArrayList<Student> seniors = new ArrayList<Student>();
-    public static ArrayList<String> senNames = new ArrayList<String>();
-    public static ArrayList<Seminar> semID;
-    public static ArrayList<Seminar> semis = new ArrayList<Seminar>();
-    public static int[][] totalSched = new int[5][5];
+    public static ArrayList<Seminar> seminars = new ArrayList<Seminar>();
+    public static ArrayList<String> stuNames = new ArrayList<String>();
+    public static ArrayList<Presenter> lecturers = new ArrayList<Presenter>();
+    public static ArrayList<String> lectNames = new ArrayList<String>();
+    public static int[][] sessIDs = {{-1,-1,-1,-1,-1},
+                                    {-1,-1,-1,-1,-1},
+                                    {-1,-1,-1,-1,-1},
+                                    {-1,-1,-1,-1,-1},
+                                    {-1,-1,-1,-1,-1}};
     public static void main (String[] args)
     {
         if (args.length != 2)
@@ -18,196 +29,281 @@ public class Tester
             return;
         }
 
-        try 
+        try
         {
-            String[] parts; // for text.split
-            //begin reading file for students
             File stuFile = new File(args[0]);
-            Scanner fileR = new Scanner(stuFile);
-            while(fileR.hasNextLine())
+            Scanner fileIn =  new Scanner(stuFile);
+
+            String[] studentIn;
+            while (fileIn.hasNextLine())
             {
-                parts = fileR.nextLine().split(",");
-                //System.out.println(parts[0]);
-                if (parts.length > 2)
-                {
-                    seniors.add(new Student(parts));
-                }
-                else
-                {
-                    seniors.add(new Student(parts[0], parts[1].strip()));
-                    //System.out.println(parts[1].strip());
-                }
-                senNames.add(parts[1].toLowerCase().strip()); // parallel for search function
+                studentIn = fileIn.nextLine().split(",");
+                seniors.add(new Student(studentIn));
+                stuNames.add(studentIn[1]);
+                System.out.println(studentIn[1]);
             }
-
-            fileR.close();
-
-            // began reading file for seminars
+            fileIn.close();
 
             File semiFile = new File(args[1]);
-            fileR = new Scanner(semiFile);
-
-            while (fileR.hasNextLine())
+            fileIn = new Scanner(semiFile);
+            String[] semiIn;
+            while (fileIn.hasNextLine())
             {
-                parts = fileR.nextLine().split(",");
-                semis.add(new Seminar(parts));
+                semiIn = fileIn.nextLine().split(",");
+                seminars.add(new Seminar(semiIn));
+                if (!lectNames.contains(semiIn[2]))
+                {
+                    lectNames.add(semiIn[2]);
+                    lecturers.add(new Presenter(semiIn[2]));
+                }
             }
 
-            fileR.close();
+            fileIn.close();
         }
         catch (FileNotFoundException e)
         {
-            System.out.println("Error: File could not be found");
+            System.out.println("Error: File could not be found.");
             return;
         }
 
-        sortSemis();
-        addChoices(); // add the choices that students wanted
-        addLeft(); //add the leftovers
+        countPrefs();
+        assignSessions();
 
-        Scanner intro = new Scanner(System.in);
-        String next;
-
+        /*for (int o = 0; o < 5; o++)
+        {
+            for(int p = 0; p < 5; p++)
+            {
+                System.out.print(sessIDs[o][p] + "\t");
+            }
+            System.out.println();
+        }*/
+        addLeft();
+        Scanner inptr = new Scanner(System.in);
+        String nextLine;
         do
         {
-            System.out.println("Actions: \n \t Find Student \n \t Print Master Roster \n \t Exit");
-            next = intro.nextLine().toLowerCase();
-            if (next.equals("find student"))
+            System.out.println("FIND STUDENT");
+            System.out.println("FIND PRESENTER");
+            System.out.println("PRINT MASTER ROSTER");
+
+            nextLine = inptr.nextLine().toLowerCase();
+
+            if (nextLine.contains("student"))
             {
-                System.out.print("Please enter student name: ");
-                next = intro.nextLine().toLowerCase();
-                if (senNames.indexOf(next) < 0)
-                    System.out.println("Error: Student not found");
+                System.out.print("ENTER STUDENT'S NAME:");
+                nextLine = inptr.nextLine().strip();
+                if (ignoreCaseStu(nextLine) == -1)
+                    System.out.println("STUDENT COULD NOT BE FOUND");
                 else
-                    System.out.println(seniors.get(senNames.indexOf(next)));
+                    System.out.println(seniors.get(ignoreCaseStu(nextLine)));
             }
-            else if (next.equals("print master roster"))
+            else if (nextLine.contains("presenter"))
             {
-                for (int r = 0; r < 5; r++)
+                System.out.print("ENTER PRESENTER'S NAME:");
+                nextLine = inptr.nextLine().strip();
+                if (ignoreCaseLect(nextLine) == -1)
+                    System.out.println("PRESENTER COULD NOT BE FOUND");
+                else
+                    System.out.println(lecturers.get(ignoreCaseLect(nextLine)));
+            }
+            else if (nextLine.contains("roster"))
+            {
+                for (int t = 0; t < 5; t++)
                 {
-                    System.out.println("Slot " + (r + 1) + ":");
-                    for (int c = 0; c < 5; c++)
-                        {
-                            System.out.println(semis.get(totalSched[r][c]).toString(r));
-                        }
-                    System.out.println("\n\n");
-                }
-            }
-            else if (!next.equals("exit"))
-            {
-                System.out.println("Error: Action not possible");
-            }
-        }
-        while (!next.equals("exit"));
-        intro.close();
-    }
-
-    public static void sortSemis() //sorts semis to be greatest to least
-    {
-        for (Student senior : seniors)
-        {
-            if (senior.madeChoices())
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    semis.get(senior.getChoice(i) - 1).addChoice();
-                }
-            }
-        }
-
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-        int inser;
-        for (Seminar sem : semis)
-        {
-            inser = temp.size();
-            while(inser > 0 && semis.get(inser-1).getPrefCt() < sem.getPrefCt())
-            {
-                inser--;
-            }
-            temp.add(inser, sem.getID());
-        }
-
-        int tempctr = 0;
-        
-        for (int c = 0; c < 5; c++)
-        {
-            for (int r = 0; r < 5; r++)
-            {
-                if(!semis.get(temp.get(tempctr)).canAdd(r))
-                {
-                   tempctr++;
-                   //System.out.println("temp: " + semis.get(temp.get(tempctr)).getName());
-                }
-                totalSched[r][c] = semis.get(temp.get(tempctr)).getID();
-                //System.out.println(totalSched[r][c]);
-                semis.get(temp.get(tempctr)).addSess(r,c);
-            }
-        }
-        temp.clear(); // seminars are no longer needed after this
-    }
-
-    public static void addChoices()
-    {
-        for (int st = 0; st < 5; st++) //cycle thru all 1st choices, then 2nd, etc etc
-        {
-            for (Student stu1 : seniors)
-            {
-                if (stu1.madeChoices())
-                {
-                    for (int a = 0; a < 5; a++)
+                    for (int u = 0; u < 5; u++)
                     {
-                        //System.out.println("trying to get semi @ " + st + "in " + a + " ID " + totalSched[st][a]);
-                        if(semis.get(totalSched[st][a]).hasSpace(st) && semis.get(totalSched[st][a]).getID() == stu1.getChoice(st) && stu1.canAdd(st, semis.get(totalSched[st][a]).getID())) //if the seminar has space at the student wants it
-                        {
-                            //System.out.println("trying to add student");
-                            semis.get(totalSched[st][a]).addStu(st, stu1.getName());
-                            stu1.assignSemi(st, semis.get(totalSched[st][a]));
-                            stu1.addedChoice(st);
-                            break;
-                        }
+                        System.out.println(seminars.get(sessIDs[t][u]).getSess(t));
                     }
                 }
             }
         }
+        while (!nextLine.equalsIgnoreCase("exit"));
+        inptr.close();
+    }
+
+    public static int ignoreCaseStu(String name)
+    {
+        int len = stuNames.size();
+        for (int v = 0; v < len; v++)
+        {
+            System.out.println(stuNames.get(v));
+            if (stuNames.get(v).equalsIgnoreCase(name))
+            {
+                return v;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int ignoreCaseLect(String name)
+    {
+        int len = lectNames.size();
+        for (int v = 0; v < len; v++)
+        {
+            if (lectNames.get(v).equalsIgnoreCase(name))
+            {
+                return v;
+            }
+        }
+
+        return -1;
+    }
+    /**tallies up the prefs of all the students */
+    public static void countPrefs()
+    {
+        for (Student f : seniors)
+        {
+            if (f.hadPrefs())
+            {
+                for (int g = 0; g < 5; g++)
+                {
+                    seminars.get(f.getPref(g)).addPref();
+                }
+            }
+        }
+    }
+
+    public static void assignSessions()
+    {
+        ArrayList<Seminar> sorted = new ArrayList<Seminar>();
+        int sortEnd;
+        for (Seminar h : seminars)
+        {
+            sortEnd = sorted.size();
+            while (sortEnd != 0)
+            {
+                if (h.getPrefCt() <= sorted.get(sortEnd-1).getPrefCt())
+                {
+                    break;
+                }
+                else
+                {
+                    sortEnd--;
+                }
+            }
+            sorted.add(sortEnd, h);
+        }
+        int time;
+        int room;
+        int curs;
+        for (int l = 0; l < 25; l++)
+        {
+            //System.out.println(l);
+            time = bestTime(sorted.get(0).getId());
+            room = findEmptyRoom(time);
+            sessIDs[time][room]= sorted.get(0).getId();
+            lecturers.get(lectNames.indexOf(sorted.get(0 ).getLectName())).assign(time, sorted.get(0).getName(), room);
+            sorted.get(0).addSession(time, room);
+
+
+            curs = 0;
+            while (curs < sorted.size() && sorted.get(0).canAddStudent(time))
+            {
+                //System.out.println("in adding student while loop");
+                if (seniors.get(curs).canAdd(time, sorted.get(0).getId()) && seniors.get(curs).hasPrefFor(sorted.get(0).getId()))
+                {
+                    ///System.out.println("added student");
+                    seniors.get(curs).assignSession(time, room, sorted.get(0));
+                    sorted.get(0).getSess(time).addStudent(seniors.get(curs));
+                }
+                curs++;
+            }
+
+            if (!sorted.get(0).canAddSession())
+            {   
+                //System.out.println(sorted.get(0).getName());
+                sorted.remove(0);
+            }
+        }
+    }
+
+    public static int bestTime(int semiID)
+    {
+        int time = -1;
+        for (int i = 0; i < 5; i++)
+        {
+            //System.out.println(lecturers.get(lectNames.indexOf(seminars.get(semiID).getLectName())).isBusy(i));
+            if (findEmptyRoom(i) != -1 && !lecturers.get(lectNames.indexOf(seminars.get(semiID).getLectName())).isBusy(i))
+            {
+                time = i;
+                break;
+            }
+        }
+        int minConflicts = Integer.MAX_VALUE;
+        int temp;
+        int sessCt;
+        for (int m = 0; m < 5; m++)
+        {
+            temp = 0;
+            sessCt = 0;
+            for (int n = 0; n < 5; n++)
+            {   
+                
+                if (sessIDs[m][n] == -1)
+                {
+                    break;
+                }
+                sessCt++;
+                temp += seminars.get(sessIDs[m][n]).getSess(m).alsoWants(semiID);
+            }
+            if (temp < minConflicts && sessCt < 5 && !lecturers.get(lectNames.indexOf(seminars.get(semiID).getLectName())).isBusy(m))
+            {
+                minConflicts = temp;
+                time = m;
+            }
+        }
+        return time;
+    }
+
+    public static int findEmptyRoom(int time)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (sessIDs[time][i] == -1)
+                return i;
+        }
+        return -1;
+    }
+
+    public static int[] findSess(int searchVal)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            for (int k = 0; k < 5; k++)
+            {
+                if (sessIDs[j][k] == searchVal)
+                {   
+                        int[] temp = {j,k};
+                        return temp;
+                }
+            }
+        }
+        int[] temp = {-1,-1};
+        return temp;
     }
 
     public static void addLeft()
     {
-        int sem;
-            for (Student stu2 : seniors)
+        for (Student q : seniors)
+        {
+            if (q.hasEmpty())
             {
-                //System.out.println(stu2.getName() + stu2.hasEverything());
-                if(!stu2.hasEverything())
+                for (int r = 0; r < 5; r++)
                 {
-                    //System.out.println("doesnt have everything");
-                    for (int t = 0; t < 5; t++)
+                    if (!q.hasSess(r))
                     {
-                        sem = 0;
-                        while (!stu2.isBusy(t))
+                        for (int s = 0; s < 5; s++)
                         {
-                            
-                            //System.out.println(stu2.getName() + " is not busy @ " + t);
-                            //System.out.println(sem);
-                            if (semis.get(totalSched[t][sem]).hasSpace(t))
+                            if (seminars.get(sessIDs[r][s]).canAddStudent(r) && q.canAdd(r, sessIDs[r][s]))
                             {
-                                //System.out.println("has space");
-                                if (stu2.canAdd(t, sem, semis.get(totalSched[t][sem]).getName()))
-                                {
-                                    stu2.assignSemi(t, semis.get(totalSched[t][sem]));
-                                    //System.out.println("trying to add student");
-                                    semis.get(totalSched[t][sem]).addStu(t, stu2.getName());
-                                    break;
-                                }
+                                seminars.get(sessIDs[r][s]).addStudent(r, q);
+                                q.assignSession(r, s, seminars.get(sessIDs[r][s]));
                             }
-                            if (sem == 4)
-                            {
-                                //System.out.print("error: overflow");
-                                    return;
-                            }
-                            sem++;
                         }
                     }
                 }
             }
         }
+    }
 }
