43511b883f15886eae286b7fd8dd47d3abcca98f
IsaDonutCat <114428942+IsaDonutCat@users.noreply.github.com>
2024-03-08 11:13:55 -0500

    electric boogaloo squared

--- Presenter.java
+++ Presenter.java
@@ -1,26 +1,39 @@
 public class Presenter
 {
+    private boolean[] isBusy = {false, false, false, false, false}; //default of boolean is false
+    private int[] roomNum = {-1,-1,-1,-1,-1};
+    private String[] presenting = new String[5];
     private String name;
-    private boolean[] isBusy = new boolean[5]; //default of boolean is false
 
-    public Presenter (String name)
+    public Presenter(String name)
     {
         this.name = name;
     }
-
-    public String getName()
-    {
-        return name;
-    }
-
     public boolean isBusy (int ind)
     {
         return isBusy[ind];
     }
 
-    public void assign(int ind)
+    public void assign(int ind, String seminar, int roomNum)
     {
         isBusy[ind] = true;
+        presenting[ind] = seminar;
+        this.roomNum[ind] = roomNum + 101;
     }
 
+    public String toString()
+    {
+        String build = "";
+        build += name.toUpperCase();
+        for (int i = 0; i < 5; i++)
+        {
+            if (roomNum[i] != -1)
+            {
+                build += "\n" + presenting[i].toUpperCase();
+                build += " in " + roomNum + " in time slot " + i;
+            }
+        }
+
+        return build;
+    };
 }
