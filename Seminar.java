188daf40155c56381b35a6d45b6bdf0ba9effc56
Isabella Wang <wangisa26@7hills.org>
2024-08-26 13:54:11 -0400

    asdfdsa

--- Seminar.java
+++ Seminar.java
@@ -7,7 +7,6 @@ public class Seminar
     private int[] rooms = {-1,-1,-1,-1,-1};
     private int prefs = 0;
     private ArrayList<String> attendees = new ArrayList<String>();
-    private int ranking;
 
     public int getPref()
     {
